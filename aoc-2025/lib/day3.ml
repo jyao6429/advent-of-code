d62fd2646a4ce1b0cf620d5d95866865d15b881b
Jason Yao <jlyao@alumni.cmu.edu>
2025-12-03 00:14:38 -0500

    3.2 start

--- aoc-2025/lib/day3.ml
+++ aoc-2025/lib/day3.ml
@@ -38,3 +38,24 @@ let%expect_test "puzzle 1 - input" =
   puzzle_1 input |> Common.print_int;
   [%expect {| 16993 |}]
 ;;
+
+let num_to_turn_on = 12
+let puzzle_2 input =
+  let banks =
+    List.map input ~f:(fun line -> String.to_list line |> List.map ~f:Char.get_digit_exn)
+  in
+  List.sum (module Int) banks ~f:max_joltage
+;;
+
+let%expect_test "puzzle 2 - example" =
+  let input = {| **REMOVED** |} in
+  let lines = String.split_lines (String.strip input) in
+  puzzle_2 lines |> Common.print_int;
+  [%expect {| 357 |}]
+;;
+
+let%expect_test "puzzle 2 - input" =
+  let input = Common.read_lines "day3.txt" in
+  puzzle_2 input |> Common.print_int;
+  [%expect {| 16993 |}]
+;;
