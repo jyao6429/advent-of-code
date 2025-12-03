cea3fa6e6ec89502aa32d90f83d8d07be245d0f9
Jason Yao <jlyao@alumni.cmu.edu>
2025-12-02 21:39:46 -0500

    day2 part 1

--- aoc-2025/lib/day2.ml
+++ aoc-2025/lib/day2.ml
@@ -8,25 +8,34 @@ let puzzle_1 input =
       | [ start_str; end_str ] -> Int.of_string start_str, Int.of_string end_str
       | _ -> failwith "Invalid range format")
   in
-  let count = List.fold ranges ~init:0 ~f:(fun count (first, last)-> 
-    let counter = ref 0 in
+  let sum = List.fold ranges ~init:0 ~f:(fun sum (first, last)-> 
+    let sum_ref = ref 0 in
     for i = first to last do
       let str = Int.to_string i in
       let len = String.length str in
       if len % 2 = 0 then (
         let half_str = String.subo str ~len:(len / 2) in
         if String.equal str (half_str ^ half_str) then
-          incr counter
+          sum_ref := !sum_ref + i
       )
     done;
-    count + !counter
+    sum + !sum_ref
     ) in
-  count
+  sum
 ;;
 
 let%expect_test "puzzle 1 - example" =
   let input = {| **REMOVED** |}
   in
   puzzle_1 input |> Printf.printf "%d\n" ;
-  [%expect {| 8 |}]
+  [%expect {| 1227775554 |}]
+;;
+
+let%expect_test "puzzle 1 - input" =
+  let input =
+     In_channel.read_all
+      "/home/jyao6429/oxcaml/advent-of-code/aoc-2025/resources/day2.txt"
+  in
+  puzzle_1 input |> Printf.printf "%d\n" ;
+  [%expect {| 29940924880 |}]
 ;;
