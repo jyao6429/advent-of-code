a55af9ccb9714b3bde787a721b17a5e461752490
Jason Yao <jlyao@alumni.cmu.edu>
2025-12-05 23:15:16 -0500

    6 template

---
+++ aoc-2025/lib/day6.ml
@@ -0,0 +1,48 @@
+open! Core
+
+let day = "day6"
+
+let example_input = Common.read_lines (day ^ "_example.txt")
+
+let prod_input = Common.read_lines (day ^ ".txt")
+let process_input input = input
+let example_input = example_input |> process_input
+let prod_input = prod_input |> process_input
+
+module Part_1 = struct
+  let solve input =
+    ignore input;
+    0
+  ;;
+
+  module%test [@name "part_1"] _ = struct
+    let%expect_test "example" =
+      solve example_input |> Common.print_int;
+      [%expect {| 0 |}]
+    ;;
+
+    let%expect_test "prod" =
+      solve prod_input |> Common.print_int;
+      [%expect {| 0 |}]
+    ;;
+  end
+end
+
+module Part_2 = struct
+  let solve input =
+    ignore input;
+    0
+  ;;
+
+  module%test [@name "part_2"] _ = struct
+    let%expect_test "example" =
+      solve example_input |> Common.print_int;
+      [%expect {| 0 |}]
+    ;;
+
+    let%expect_test "prod" =
+      solve prod_input |> Common.print_int;
+      [%expect {| 0 |}]
+    ;;
+  end
+end
