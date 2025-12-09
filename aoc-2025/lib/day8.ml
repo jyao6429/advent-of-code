7142a35fba6107ac1fa6f71c752474dc8526dd7a
Jason Yao <jlyao@alumni.cmu.edu>
2025-12-08 23:22:41 -0500

    8.1

--- aoc-2025/lib/day8.ml
+++ aoc-2025/lib/day8.ml
@@ -60,7 +60,7 @@ let process_input input =
           })
     |> Set.of_list (module Edge)
   in
-  ~edges, ~num_coords:(List.length coords)
+  ~edges, ~coords
 ;;
 
 let example_input = example_input |> process_input
@@ -68,92 +68,66 @@ let prod_input = prod_input |> process_input
 
 module Union_find = struct
   module Node = struct
-    type 'a canon =
-      { size : int
-      ; value : 'a
-      }
-    [@@deriving sexp_of]
-
-    type 'a child = { mutable parent : 'a
-          ; value : 'a
-          } [@@deriving sexp_of]
-
     type 'a t =
-      | Canonical of 'a canon
-      | Child of 'a child
-          
+      { mutable parent : 'a
+      ; mutable size : int
+      }
     [@@deriving sexp_of]
   end
 
   type t = Coordinate.t Node.t Coordinate.Table.t
 
-  let create = Coordinate.Table.create
-
-  let singleton (t : t) value =
-    Hashtbl.update_and_return
-      t
-      value
-      ~f:(Option.value ~default:(Node.Canonical { size = 1; value }))
+  let create coords =
+    List.map coords ~f:(fun coord -> coord, { Node.parent = coord; size = 1 })
+    |> Hashtbl.of_alist_exn (module Coordinate)
   ;;
 
-  let canonical t = function
-    | Node.Canonical _ as node -> node
-    | Child c ->
-      let rec loop node acc =
-        match node with
-        | Node.Canonical canon ->
-          List.iter acc ~f:(fun
-             c -> c.Node.parent <- canon.value
-            );
-          node
-        | Child c -> loop (Hashtbl.find_exn t c.parent) (c :: acc)
-      in
-      loop (Child c) []
+  let make_set (t : t) coord =
+    Hashtbl.set t ~key:coord ~data:{ Node.parent = coord; size = 1 }
   ;;
 
-  let union t n1 n2 =
-    match n1, n2 with
-    | Node.Canonical can1, Node.Canonical can2 ->
-      print_s
-        [%message
-          "unioning" (can1 : Coordinate.t Node.canon) (can2 : Coordinate.t Node.canon)];
-      if can1.size < can2.size
-      then
-        Hashtbl.set
-          t
-          ~key:can1.value
-          ~data:(Node.Child { parent = can2.value; value = can1.value })
-      else
-        Hashtbl.set
-          t
-          ~key:can2.value
-          ~data:(Node.Child { parent = can1.value; value = can2.value })
-    | _ -> failwith "invalid"
+  let rec find_set t coord =
+    match Hashtbl.find_exn t coord with
+    | { Node.parent; _ } as node when Coordinate.equal coord parent -> node
+    | { Node.parent; _ } as node ->
+      let canon = find_set t parent in
+      node.parent <- canon.parent;
+      canon
+  ;;
+
+  let union t c1 c2 =
+    (* print_s
+      [%message
+        "union"
+          (t : Coordinate.t Node.t Coordinate.Table.t)
+          (c1 : Coordinate.t)
+          (c2 : Coordinate.t)]; *)
+    let n1, n2 = find_set t c1, find_set t c2 in
+    if [%equal: Coordinate.t] n1.Node.parent n2.Node.parent |> not
+    then (
+      let smaller, larger = if n1.Node.size < n2.Node.size then n1, n2 else n2, n1 in
+      smaller.Node.parent <- larger.Node.parent;
+      larger.Node.size <- larger.Node.size + smaller.Node.size)
   ;;
 end
 
 module Part_1 = struct
-  let solve (~edges, ~num_coords) =
-    let uf = Union_find.create () in
+  let solve (~edges, ~coords) ~num_connections =
+    let uf = Union_find.create coords in
     let _ =
-      List.range 0 num_coords
+      List.range 0 num_connections
       |> List.fold ~init:edges ~f:(fun edges _ ->
         let min, edges = Set.min_elt_exn edges, Set.remove_index edges 0 in
-        let can1, can2 =
-          ( Union_find.singleton uf min.Edge.v1 |> Union_find.canonical uf
-          , Union_find.singleton uf min.Edge.v2 |> Union_find.canonical uf )
-        in
-        Union_find.union uf can1 can2;
+        Union_find.union uf min.Edge.v1 min.Edge.v2;
         edges)
     in
-    print_s
+    (* print_s
       [%message
-        (Hashtbl.to_alist uf : (Coordinate.t * Coordinate.t Union_find.Node.t) list)];
+        (Hashtbl.to_alist uf : (Coordinate.t * Coordinate.t Union_find.Node.t) list)]; *)
     let circuit_sizes =
       Hashtbl.to_alist uf
-      |> List.filter_map ~f:(function
-        | _, Union_find.Node.Canonical { size; _ } -> Some size
-        | _ -> None)
+      |> List.filter_map ~f:(fun (coord, node) ->
+        if Coordinate.equal coord node.parent then Some node.size else None)
     in
     let sorted_decreasing = List.sort circuit_sizes ~compare:(fun a b -> compare b a) in
     let top3 = List.take sorted_decreasing 3 in
@@ -162,14 +136,14 @@ module Part_1 = struct
 
   module%test [@name "part_1"] _ = struct
     let%expect_test "example" =
-      solve example_input |> Common.print_int;
-      [%expect {| 1 |}]
+      solve example_input ~num_connections:10 |> Common.print_int;
+      [%expect {| 40 |}]
     ;;
 
-    (* let%expect_test "prod" =
-      solve prod_input |> Common.print_int;
-      [%expect {| 0 |}]
-    ;; *)
+    let%expect_test "prod" =
+      solve prod_input ~num_connections:1000 |> Common.print_int;
+      [%expect {| 75582 |}]
+    ;;
   end
 end
 
